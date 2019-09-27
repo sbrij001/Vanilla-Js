062ccf20b7d88c7031da20dd48998c006036d007
sbrij001 <sbrij001@yahoo.com>
2019-09-27 12:27:56 -0400

    notes on understanding DOM tree structure

---
+++ DOM-manipulation/index.js
@@ -0,0 +1,8 @@
+// DOM NOTES
+
+// the dom is bascially an upside down tree
+// the roots are at the top of the dome
+// and the leaves are the deeply nested HTML elements
+
+// every tree contains subtrees
+// these subtrees are independent from their parent trees
